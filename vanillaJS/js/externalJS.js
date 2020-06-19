0b06f9b2f0c1ee6ae0bd9fd0f682a012c5bf78e0
VictorJP <victor.jaimes.puente@gmail.com>
2020-06-18 22:52:17 -0500

    js variable practice

--- vanillaJS/js/externalJS.js
+++ vanillaJS/js/externalJS.js
@@ -1,2 +1,18 @@
 "use strict"
-console.log('hello world');
+console.log('hello worldb1');
+function logHello() {
+    var output = console.log("hello World")
+    return output;
+}
+logHello();
+
+// Variable practice
+
+function increment(x) {
+    return x + 1;
+}
+
+var four = increment(3);
+var six = increment(increment(increment(3)));
+
+document.write(four);
